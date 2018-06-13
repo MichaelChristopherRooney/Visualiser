1ee1a391b8022180e82547691a9ea891974dd960
Michael Rooney <MikeyRooney@users.noreply.github.com>
2018-06-13 10:14:59 +0100

    Now displays iterations progressing

--- Visualiser/src/globals.hpp
+++ Visualiser/src/globals.hpp
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+#include "sphere.hpp"
+
 extern float grid_x_start;
 extern float grid_x_end;
 extern float grid_y_start;
@@ -10,5 +12,7 @@ extern float grid_z_start;
 extern float grid_z_end;
 
 extern uint64_t num_spheres;
-
+extern float simulation_time_of_last_event;
+extern float simulation_time_of_next_event;
 extern uint64_t iteration_num;
+extern struct sphere_s *spheres;
